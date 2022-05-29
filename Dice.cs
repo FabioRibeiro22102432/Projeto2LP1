358bc106bc2428b8037da504ae29b061d0c80432
FabioRibeiro22102432 <fabioribeiro.info@gmail.com>
2022-05-29 14:49:26 +0100

    Documentetion XML for Dice class

--- Dice.cs
+++ Dice.cs
@@ -7,6 +7,11 @@ namespace Projeto2
 {
     public class Dice
     {
+        // Roll dice (1 - 6)
+        /// <summary>
+        ///  Number generator (die) 1-6
+        /// </summary>
+        /// <returns>random number between 1-6</returns>
          public int dice(Random rand)                   
         {
         int dice = rand.Next(1, 7);
